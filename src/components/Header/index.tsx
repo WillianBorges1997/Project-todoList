32d1a4812afb188f14c14f995dba6560911c0849
Willian <willianedi@hotmail.com>
2023-04-19 22:45:49 -0300

    Add existing file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 import Logo from "../../assets/Logo.svg";
 
+import styles from './header.module.css'
+
 export function Header() {
 return ( 
-<header>
+<header className={styles.header}>
 <img src={Logo} />
+
+<form className={styles.newOrderClass}>
+<input
+placeholder="Adicione uma nova tarefa"/>
+<button>Criar</button>
+</form>
 </header>
 );
 }
