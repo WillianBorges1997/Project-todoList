3d958bde9fefd4394c81decd0340e7badd4bc94e
Willian <willianedi@hotmail.com>
2023-04-27 01:23:03 -0300

    inclusão

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -3,47 +3,59 @@ import { Task } from '../Task';
 import styles from './tasks.module.css';
 
 
-const list =[
+const list = [
+
+
     {
-    id: 1,
-    title: 'estudar',
-    isComplete: false
+        id: 1,
+        title: 'estudar',
+        isComplete: false
     },
+   
     {
         id: 2,
         title: 'correr',
         isComplete: false
-    },
+    }, 
     {
         id: 3,
         title: 'dormir',
         isComplete: false
         },
-     
-]
+                                                    
+];
+/*
+const result = list.filter(lists => lists.length > 3);
+
+console.log(result);  //resultado 
+
+*/
+
 export function Tasks() {
+
+
     
-return(
-<section className={styles.tasks}>
-
-<header className={styles.header}>
-
-    <div>
-        <p>Tarefas Criadas</p>
-        <span>10</span>
+    return(
+    <section className={styles.tasks}>
+    
+    <header className={styles.header}>
+    
+        <div>
+            <p>Tarefas Criadas</p>
+            <span>10</span>
+        </div>
+        <div>
+            <p className={styles.colorPurple}>Concluídas</p>
+            <span>2 de 10</span>
+        </div>
+    </header>
+    
+    <div className={styles.imput}>
+        {list.map( Header =>  { 
+            return <Task />
+        })}
+       
     </div>
-    <div>
-        <p className={styles.colorPurple}>Concluídas</p>
-        <span>2 de 10</span>
-    </div>
-</header>
-
-<div className={styles.imput}>
-    {list.map( Header =>  { 
-        return <Task />
-    })}
-   
-</div>
-</section>
-)
-}
+    </section>
+    )
+    }
